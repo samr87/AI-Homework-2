729c96c2e819c4f9d6a014227b02fcb11ae1ed27
June-Tea <70388619+June-Tea@users.noreply.github.com>
2020-09-09 22:39:57 -0400

    Create Experiment 2.py

---
+++ Experiment 2.py
@@ -0,0 +1,28 @@
+import random
+from Agent import Agent
+avg = 0
+def main():
+    environment = [[random.randint(0,1) for i in range(0,10)] for j in range(0,10)]
+    roomba = Agent(random.randint(0,9),random.randint(0,9))
+    p = 0
+    for i in environment:
+        for j in i:
+            p += j
+    for i in range(0,50):
+        x = roomba.GetLocationX()
+        y = roomba.GetLocationY()
+        if environment[x][y] != 0:
+            environment[x][y] = 0
+        roomba.MoveRandom()
+    pe = 0
+    for i in environment:
+        for j in i:
+            pe += j
+    pe = p - pe
+    performance = (pe/p)*100
+    print("Perfomance: %.0f%% cleaner" %performance)
+    return performance
+for i in range(0,100):
+    avg += main()
+avg = avg/100
+print("Average performance: %.0f%%" %avg)
