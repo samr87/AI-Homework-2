cd01e6e00fbb8166361e034ecba14442f947856d
June-Tea <70388619+June-Tea@users.noreply.github.com>
2020-09-09 20:35:53 -0400

    Create Agent.py

---
+++ Agent.py
@@ -0,0 +1,26 @@
+from random import randint
+class Agent():
+    def __init__ (self):
+        self.name = ""
+        self.__age = randint(0,100)
+        self.__locationX = 0
+        self.__locationY = 0 
+    def setName(self,n):
+        self.name = n
+    def getName(self):
+        return self.name
+    def MoveRandom(self):
+        self.__locationY = randint(0,10)
+        self.__locationX = randint(0,10)
+    def MoveUp(self):
+        self.__locationY += 1
+    def MoveDown(self):
+        self.__locationY -= 1
+    def MoveLeft(self):
+        self.__locationX -= 1
+    def MoveRight(self):
+        self.__locationX += 1
+    def GetLocationX(self):
+        return self.__locationX
+    def GetLocationY(self):
+        return self.__locationY
